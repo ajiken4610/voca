79569bd2df17bf2acafa29823871e8777dfdfb4c
ajiken4610 <ajiken4610@gmail.com>
2023-01-12 21:11:27 +0900

    change primary color

--- plugins/balmui.ts
+++ plugins/balmui.ts
@@ -3,6 +3,6 @@ import BalmUIPlus from "balm-ui/dist/balm-ui-plus";
 import "balm-ui/dist/balm-ui.css";
 
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(BalmUI);
+  nuxtApp.vueApp.use(BalmUI, { $theme: { primary: "#0088cc" } });
   nuxtApp.vueApp.use(BalmUIPlus);
 });
